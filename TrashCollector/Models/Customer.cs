09314f0b7c6e964f848af1b3162179e8aa89d8e3
Corey Rome <coreyrome2@gmail.com>
2018-01-03 14:13:49 -0600

    updated customer properties

--- TrashCollector/Models/Customer.cs
+++ TrashCollector/Models/Customer.cs
@@ -6,9 +6,12 @@ using System.Web;
 
 namespace TrashCollector.Models
 {
-    public class Customer
+    public class Customer : Address
     {
         [Key]
         public int CustomerId { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+        public int CustomerDay { get; set; }
     }
 }
