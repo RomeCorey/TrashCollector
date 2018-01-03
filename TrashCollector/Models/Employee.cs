a11de761ce425f5877625218040454942538e4af
Corey Rome <coreyrome2@gmail.com>
2018-01-03 10:20:24 -0600

    properties for classes added

--- TrashCollector/Models/Employee.cs
+++ TrashCollector/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,7 @@ namespace TrashCollector.Models
 {
     public class Employee
     {
+        [Key]
+        public int EmployeeId { get; set; }
     }
 }
