977ccbf19e1617caa4d98bcc9e0be8e96fbe0934
Corey Rome <coreyrome2@gmail.com>
2018-01-03 09:21:24 -0600

    added some classes and controllers

--- TrashCollector/Models/IdentityModels.cs
+++ TrashCollector/Models/IdentityModels.cs
@@ -29,5 +29,7 @@ namespace TrashCollector.Models
         {
             return new ApplicationDbContext();
         }
+
+        public System.Data.Entity.DbSet<TrashCollector.Models.Day> Days { get; set; }
     }
 }
